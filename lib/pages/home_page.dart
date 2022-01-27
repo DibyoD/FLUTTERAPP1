03fd83dc0f791929c5e48746a23d53e73ea6dc25
Dibyo Dhara <dibyodhara1998@gmail.com>
2022-01-28 01:25:34 +0530

    day 4 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const int day = 3;
+    const int day = 4;
     return Scaffold(
       appBar: AppBar(
         title: Text("FirstApp"),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,11 +6,46 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",
-            style: TextStyle(
-                fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold)),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_img.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    //obscuringCharacter: "*",
+                    decoration: const InputDecoration(
+                        labelText: "Password", hintText: "Enter Password"),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Hello!!");
+                      },
+                      child: const Text("Login"))
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
